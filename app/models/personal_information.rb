bb8d93995191cce9b3a53193cbd97ba143a49486
Daniel Harsojo <patccca@host.ccca.org.au>
2010-07-21 21:36:53 +1000

    Added custom legal first and last name fields

--- app/models/personal_information.rb
+++ app/models/personal_information.rb
@@ -14,6 +14,7 @@ class PersonalInformation < CustomElement
     # options, it messes up the update_attributes save since it's a defined
     # method and not a column
     if params[:appln_person]
+      
       # copy local address info to permanent if requested
       copy = params[:appln_person].delete :permanent_same_as_local
       if copy == '1'
@@ -24,6 +25,15 @@ class PersonalInformation < CustomElement
       end
 
       person_params = params[:appln_person].clone
+      
+      # we need to fill in legal first name & last name if it wasn't filled in already
+      if person_params[:first_name].empty?
+        person_params[:first_name] = person_params[:preferred_first_name]
+      end
+      if person_params[:last_name].empty?
+        person_params[:last_name] = person_params[:preferred_last_name]
+      end
+
       person_params.delete 'email'
       person_params.delete 'year_in_school_id'
 
